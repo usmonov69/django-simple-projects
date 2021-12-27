68899cef368bb0662d67e958b835996c65d7ad92
usmonov69 <usmonovraxmat600@gmail.com>
2021-12-27 00:40:12 +0000

    index and barcode_img  func added for frontend

--- barcode_proj/products/views.py
+++ barcode_proj/products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect , get_object_or_404
+from django.urls import reverse
+from .models import Product
+from .forms import ProductForm
 
-# Create your views here.
+
+def index(request):
+	form = ProductForm(request.POST or None)
+	if request.method == 'POST':
+		if form.is_valid():
+			form.save()
+			return redirect(reverse('barcode-img', kwargs={
+				'id': form.instance.id
+				}))
+
+	return render(request, 'index.html', {'form':form})
+
+
+
+def barcode_img(request, id):
+	objects = get_object_or_404(Product, id=id)
+	return render(request, 'barcode.html', {'objects':objects})
